78c95107fe5d383c4296a48a7d7f4b893a8312a2
Joel Nathe <nathe031@umn.edu>
2023-04-13 22:14:31 -0500

    Added datacollector class

---
+++ libs/transit/include/DataCollector.h
@@ -0,0 +1,39 @@
+#ifndef DATA_COLLECTOR_H
+#define DATA_COLLECTOR_H
+
+#include <string>
+#include <vector>
+
+class DataCollector {
+    public:
+        //It's what makes a singleton, a singleton
+        static DataCollector* getInstance(){
+            if (instance == nullptr){
+                instance = new DataCollector();
+            }
+            return instance;
+        }
+
+        //Can change input parameters if needed
+        void addData(std::vector<std::string> newData);
+
+        void toCSV(std::string fileName);
+
+        //No copying
+        DataCollector(DataCollector &other) = delete;
+
+        void operator=(const DataCollector &) = delete;
+
+        //For debugging-possibly change to print
+        std::vector<std::vector<std::string>> getData(){return data;}
+
+    private:
+        DataCollector(){};
+        static DataCollector* instance;
+        std::vector<std::vector<std::string>> data;
+};
+
+DataCollector* instance = nullptr;
+
+
+#endif
---
+++ libs/transit/src/DataCollector.cc
@@ -0,0 +1,7 @@
+#include "DataCollector.h"
+
+
+void DataCollector::addData(std::vector<std::string> newData){}
+
+void DataCollector::toCSV(std::string fileName){}
+
