a6db24ee33f5b496d3bb5f8a8b13608b62b13b0a
Asahi Kuroki <kurok011@umn.edu>
2023-04-14 17:04:08 -0500

    add implementation for datacollector

--- libs/transit/src/DataCollector.cc
+++ libs/transit/src/DataCollector.cc
@@ -1,7 +1,18 @@
 #include "DataCollector.h"
 
 
-void DataCollector::addData(std::vector<std::string> newData){}
-
-void DataCollector::toCSV(std::string fileName){}
+void DataCollector::addData(std::vector<std::string> newData){
+    data.push_back(newData);
+}
+void DataCollector::toCSV(std::string fileName){
+    std::ofstream file;
+    file.open(fileName);
+    for (auto row : data){
+        for (auto col : row){
+            file << col << ",";
+        }
+        file << std::endl;
+    }
+    file.close();
+}
 
