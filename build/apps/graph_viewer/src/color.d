71dd83a6e65f7a31c7ac460b5611e791a66cc098
Asahi Kuroki <kurok011@umn.edu>
2023-05-02 20:52:32 -0500

    checking if everything works

--- build/apps/graph_viewer/src/color.d
+++
@@ -1,3 +0,0 @@
-../../build/apps/graph_viewer/src/color.o: src/color.cc src/color.h
-
-src/color.h:
--- build/libs/libtransit.a/src/math/vector3.d
+++
@@ -1,4 +0,0 @@
-../../build/libs/libtransit.a/src/math/vector3.o: src/math/vector3.cc \
- include/math/vector3.h
-
-include/math/vector3.h:
